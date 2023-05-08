4b227542e13bd4e1c6c2d34c6c4c67aa8df61e3e
mscholz.dev <mscholz.dev@gmail.com>
2023-05-08 17:08:57 +0200

    test

--- utils/Tool.ts
+++ utils/Tool.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 export default class Tool {
   readFileSync(filePath: string[]): string {
+    console.log(process.cwd());
     return readFileSync(
       path.join(process.cwd(), ...filePath),
       "utf8",
