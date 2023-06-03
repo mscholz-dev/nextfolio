c7b51a029559de3bea58c562f4735d23ef4cec72
mscholz.dev <mscholz.dev@gmail.com>
2023-06-03 14:09:43 +0200

    change link in footer

--- utils/data/socialNetworks.tsx
+++ utils/data/socialNetworks.tsx
@@ -1,8 +1,6 @@
 import IconLinkedin from "@/public/icons/linkedin.svg";
 import IconGithub from "@/public/icons/github.svg";
-import IconDiscord from "@/public/icons/discord.svg";
 import IconInstagram from "@/public/icons/instagram.svg";
-import IconTwitter from "@/public/icons/twitter.svg";
 import IconYoutube from "@/public/icons/youtube.svg";
 
 const socialNetworks = [
@@ -14,34 +12,22 @@ const socialNetworks = [
   },
   {
     id: 1,
-    icon: <IconDiscord />,
-    url: "https://discord.gg/qpE47S6r",
-    title: "Serveur Discord de Morgan Scholz",
-  },
-  {
-    id: 2,
     icon: <IconLinkedin />,
     url: "https://www.linkedin.com/in/morgan-scholz/",
     title: "Profil Linkedin de Morgan Scholz",
   },
   {
-    id: 3,
+    id: 2,
     icon: <IconGithub />,
     url: "https://github.com/mscholz-dev",
     title: "Profil Github de Morgan Scholz",
   },
 
   {
-    id: 4,
+    id: 3,
     icon: <IconInstagram />,
     url: "https://www.instagram.com/mscholz.dev/",
     title: "Profil Instagram de Morgan Scholz",
-  },
-  {
-    id: 5,
-    icon: <IconTwitter />,
-    url: "https://twitter.com/mscholz_dev",
-    title: "Profil Twitter de Morgan Scholz",
   },
 ];
 
