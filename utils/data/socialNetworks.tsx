2c5960c9cf0f40a322eda75781af65ef3a10267f
mscholz.dev <mscholz.dev@gmail.com>
2023-06-03 13:34:28 +0200

    change footer social links

--- utils/data/socialNetworks.tsx
+++ utils/data/socialNetworks.tsx
@@ -2,38 +2,46 @@ import IconLinkedin from "@/public/icons/linkedin.svg";
 import IconGithub from "@/public/icons/github.svg";
 import IconDiscord from "@/public/icons/discord.svg";
 import IconInstagram from "@/public/icons/instagram.svg";
-import IconRootMe from "@/public/icons/root-me.svg";
+import IconTwitter from "@/public/icons/twitter.svg";
+import IconYoutube from "@/public/icons/youtube.svg";
 
 const socialNetworks = [
   {
     id: 0,
+    icon: <IconYoutube />,
+    url: "https://www.youtube.com/channel/UCt8pZeMk5xBHr0P_5r6FRBg",
+    title: "Chaîne Youtube de Morgan Scholz",
+  },
+  {
+    id: 1,
+    icon: <IconDiscord />,
+    url: "https://discord.gg/qpE47S6r",
+    title: "Serveur Discord de Morgan Scholz",
+  },
+  {
+    id: 2,
     icon: <IconLinkedin />,
     url: "https://www.linkedin.com/in/morgan-scholz/",
     title: "Profil Linkedin de Morgan Scholz",
   },
   {
-    id: 1,
+    id: 3,
     icon: <IconGithub />,
     url: "https://github.com/mscholz-dev",
     title: "Profil Github de Morgan Scholz",
   },
+
   {
-    id: 2,
-    icon: <IconDiscord />,
-    url: "https://discord.com/users/868905501973442571",
-    title: "Profil Discord de Morgan Scholz",
-  },
-  {
-    id: 3,
+    id: 4,
     icon: <IconInstagram />,
     url: "https://www.instagram.com/mscholz.dev/",
     title: "Profil Instagram de Morgan Scholz",
   },
   {
-    id: 4,
-    icon: <IconRootMe />,
-    url: "https://www.root-me.org/mscholz-dev",
-    title: "Profil Root Me de Morgan Scholz",
+    id: 5,
+    icon: <IconTwitter />,
+    url: "https://twitter.com/mscholz_dev",
+    title: "Profil Twitter de Morgan Scholz",
   },
 ];
 
